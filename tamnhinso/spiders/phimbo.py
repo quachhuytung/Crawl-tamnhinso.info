1c58816b65bc9b683fe71cbd3b64be6e493aea85
quachhuytung <huytung.9729@gmail.com>
2019-04-26 20:15:03 +0700

    fix conflict

--- tamnhinso/spiders/phimbo.py
+++ tamnhinso/spiders/phimbo.py
@@ -72,11 +72,7 @@ class PhimleSpider(scrapy.Spider):
 
     def get_episode(self, response):
         l = ItemLoader(item=Episode(), response=response)
-<<<<<<< HEAD
         l.add_value('ep_num', response.url, re = r'tap\-(\d+)')
-=======
-        l.add_value('ep_num', response.url, re = r'\-(\d+)')
->>>>>>> eaf6d477aa6c754c86deb985bc47d456e57e424f
         l.add_value('link', self.get_link(response))
         return dict(l.load_item())
             
@@ -88,9 +84,3 @@ class PhimleSpider(scrapy.Spider):
       l.add_xpath('link_1080',  "//source[@label='1080']/@src")
       
       return dict(l.load_item())
-
-<<<<<<< HEAD
-  
-=======
-  
->>>>>>> eaf6d477aa6c754c86deb985bc47d456e57e424f
